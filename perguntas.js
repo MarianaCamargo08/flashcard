bdaac8ecf6230557edefaa2dfa29a39e1c545a9a
MarianaCamargo08 <camargo.lima.mariana@escola.pr.gov.br>
2025-08-27 12:24:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Enfermagem',
+    'O que faz um enfermeiro no seu dia a dia ',
+    'Cuida dos seus pacientes e garante seu bem estar '
+)
+
+criaCartao(
+    'Enfermagem',
+    'Um enfermeiro pode prescrever medicamentos?',
+    'Sim, em casos de emergencias'
+)
+
+criaCartao(
+    'Programação',
+    'O que é programação',
+    'criar instruçoes para o computador e executar tarefas '
+)
+
+criaCartao(
+    'Lingua inglesa',
+    ' Inglês pode ajudar na carreira?',
+    'sim'
+)
